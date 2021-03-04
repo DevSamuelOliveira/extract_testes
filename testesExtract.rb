64ef632ad9b071a93ed1a9a7ed4e035ebb96429a
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-04 12:58:35 -0300

    Reajustes na documentação

--- testesExtract.rb
+++ testesExtract.rb
@@ -12,7 +12,7 @@ s3 = Aws::S3::Resource.new(
 
 bucket = s3.bucket("extrair") #object que é refenciado com a variavel s3 e pega como parametro o nome que o bucket
 publico sera criado. OBS: a bucket tem que ser criada assim para poder ser extraida depois. Ou tornala publica na aws
-bucket.create #Aplica a creação
+bucket.create #Aplica a criação
 
 name = File.basename 'path absoluto' #pega o nome do arquivo
 obj = s3.bucket('nome do bucket').object('nome do objeto(como ele vai ficar lá no s3') #cria uma conexão com o objeto para fazer upload
@@ -20,13 +20,13 @@ obj.upload_file('path completo(já pegado na variavel name)') #faz upload do arq
 
 =end
 
-client = Aws::Textract::Client.new({
+client = Aws::Textract::Client.new({ #Faz uma conexão com a aws usando o Textract. Obs: só esta funcionando a região aplicada.
     region: 'us-west-2',
     access_key_id: 'key',
     secret_access_key: 'key'
 })
 
-client.start_document_text_detection({
+client.start_document_text_detection({ #inicia uma detecção assincrona e retorna um objeto job_id
     document_location: { # required
       s3_object: {
         bucket: "extrair",
